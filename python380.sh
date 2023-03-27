b54d1716e62f3c9f1c8fcd078cd9cdf21a07fc55
Alejandro |【Brainiak】 <101867702+thebrainiak@users.noreply.github.com>
2023-03-27 14:05:41 +0200

    Update python380.sh

--- python380.sh
+++ python380.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 echo -e "Eliminando Version actual de Python e Instalando y Compilando Python 3.8.0"
 sudo apt purge --remove python3
+sudo apt autoremove
 sudo apt update -y && sudo apt upgrade -y
 		sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev      libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
 wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
